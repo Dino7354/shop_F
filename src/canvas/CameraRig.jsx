512a1f072077bb3430cb3c31d10ed23b58b5f2a0
Dino7354 <strokeboy24@gmail.com>
2025-04-16 22:27:27 +0530

    second commit

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -21,6 +21,8 @@ const CameraRig = ({ children }) => {
       else targetPosition = [0, 0, 2];
     }
     // set the camera position and rotation
+easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, state.pointer.x / 5, 0],
